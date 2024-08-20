9e70020a43d2363a10be836a792ceddfa982a27f
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-08-20 03:57:19 -0300

    Usuario Mapping

--- src/main/java/br/com/pierre/sigta/model/Usuario.java
+++ src/main/java/br/com/pierre/sigta/model/Usuario.java
@@ -1,35 +1,65 @@
 package br.com.pierre.sigta.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+@Entity
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@Column(nullable = false, length = 100)
 	private String nome;
+	@Column(nullable = false, unique = true, length = 255)
 	private String email;
+	@Column(nullable = false)
 	private String senha;
 	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getSenha() {
 		return senha;
 	}
+	
 	public void setSenha(String senha) {
-		this.senha = senha;
+		this.senha = hashSenha(senha);
 	}
 	
+	// Método para hash da senha
+    private String hashSenha(String senha) {
+        return BCrypt.hashpw(senha, BCrypt.gensalt());
+    }
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + "]";
+	}
 	
 }
