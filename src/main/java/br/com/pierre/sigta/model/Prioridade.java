b3b557c393dbc5b3782e919c145676d5bbb73813
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-08-20 03:46:18 -0300

    Basic Models

---
+++ src/main/java/br/com/pierre/sigta/model/Prioridade.java
@@ -0,0 +1,5 @@
+package br.com.pierre.sigta.model;
+
+public enum Prioridade {
+	BAIXA, MEDIA, ALTA;
+}
---
+++ src/main/java/br/com/pierre/sigta/model/Tarefa.java
@@ -0,0 +1,63 @@
+package br.com.pierre.sigta.model;
+
+import java.time.LocalDateTime;
+
+public class Tarefa {
+	private Long id;
+	private String titulo;
+	private String descricao;
+	private Usuario responsavel;
+	private Prioridade prioridade;
+	private LocalDateTime dataHora;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Usuario getResponsavel() {
+		return responsavel;
+	}
+	
+	public void setResponsavel(Usuario responsavel) {
+		this.responsavel = responsavel;
+	}
+	
+	public Prioridade getPrioridade() {
+		return prioridade;
+	}
+	
+	public void setPrioridade(Prioridade prioridade) {
+		this.prioridade = prioridade;
+	}
+	
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+	
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+	
+	
+	
+}
